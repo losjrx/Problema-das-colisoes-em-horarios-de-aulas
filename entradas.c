44aa35959d53ef58f105d114c90da4e82bb93ba1
Lauis de Oliveira <losjrx@gmail.com>
2023-06-05 17:07:13 -0300

    Organizando o código em arquivos.

---
+++ entradas.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "entradas.h"
+
+DadosEntrada parseDadosEntrada(char *linha) {
+    DadosEntrada entrada;
+    sscanf(linha, "%d, %d, '%[^']', '%[^']', %d, '%[^']', %d, %d, %d, '%[^']', %d, %d, '%[^']', %d, '%[^']', %d, '%[^']', %d, '%[^']', %d, %d, %d, '%[^']', %d, '%[^']', %d, %d, %d, %d, %d",
+           &entrada.id, &entrada.idSolution, entrada.solutionName, entrada.solutionInitials, &entrada.idTeacher, entrada.teacherName, &entrada.idDay,
+           &entrada.idInstitution, &entrada.idUnit, entrada.unitName, &entrada.idUnitCourse, &entrada.idCourse, entrada.courseName, &entrada.idClass,
+           entrada.className, &entrada.idDiscipline, entrada.disciplineName, &entrada.idRoom, entrada.roomName, &entrada.studentsNumber,
+           &entrada.sequence, &entrada.idBeginSlot, entrada.beginTimeName, &entrada.idEndSlot, entrada.endTimeName, &entrada.idYear, &entrada.idTerm,
+           &entrada.idCollisionType, &entrada.collisionLevel, &entrada.collisionSize);
+
+    return entrada;
+}
+
+int criarLista(noDescritor** noD){
+    noDescritor* q;
+    
+    q=(noDescritor*)malloc(sizeof(noDescritor));
+    if(q==NULL){printf("MEMORIA INSUFICIENTE\n"); return(FALHA);}
+
+    q->first = NULL;
+    q->last = NULL;
+    q->qtdDados = 0;
+    (*noD) = q;
+
+    return(SUCESSO);
+}
+
+int insOrdemRecebida(noDescritor** noD, DadosEntrada celula){
+
+    struct dado* q;
+    struct dado* r;
+    
+    q=(dado*)malloc(sizeof(dado));
+    if(q==NULL){printf("MEMORIA INSUFICIENTE\n"); return(FALHA);} else {
+        
+        (*noD)->qtdDados++;
+        q->chave=(*noD)->qtdDados;
+        q->solution=celula;
+        
+        if((*noD)->first==NULL){
+            q->prox=NULL;
+            (*noD)->first=q;
+            (*noD)->last=q;
+        } else {
+            q->prox=NULL;
+            r=(*noD)->last;
+            r->prox=q;
+            (*noD)->last=q;
+        }
+    }
+    return(SUCESSO);
+}
+
+int lerDados(noDescritor** noD){
+
+    FILE *file = fopen("dados.csv", "r");
+    if (file == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return FALHA;
+    }
+
+    int aux = criarLista(noD);
+    if(!aux){
+        printf("Erro na construção da lista.");
+        exit(FALHA);
+    }
+
+    char linha[TAMANHO_MAX_STRING];
+
+    while (fgets(linha, sizeof(linha), file) != NULL) {
+
+        DadosEntrada entrada = parseDadosEntrada(linha);
+        aux = insOrdemRecebida(noD, entrada);
+
+        if(!aux){
+            printf("Erro na construção da lista.");
+            exit(1);
+        }
+    }
+
+    fclose(file);
+
+    return SUCESSO;
+}
+
+void printarDados(noDescritor** noD){
+    struct dado* q;
+
+    q=(*noD)->first;
+
+    while(q != NULL){
+
+        printf("id: %d\n", q->solution.id);
+        printf("idSolution: %d\n", q->solution.idSolution);
+        printf("solutionName: %s\n", q->solution.solutionName);
+        printf("solutionInitials: %s\n", q->solution.solutionInitials);
+        printf("idTeacher: %d\n", q->solution.idTeacher);
+        printf("teacherName: %s\n", q->solution.teacherName);
+        printf("idDay: %d\n", q->solution.idDay);
+        printf("idInstitution: %d\n", q->solution.idInstitution);
+        printf("idUnit: %d\n", q->solution.idUnit);
+        printf("unitName: %s\n", q->solution.unitName);
+        printf("idUnitCourse: %d\n", q->solution.idUnitCourse);
+        printf("idCourse: %d\n", q->solution.idCourse);
+        printf("courseName: %s\n", q->solution.courseName);
+        printf("idClass: %d\n", q->solution.idClass);
+        printf("className: %s\n", q->solution.className);
+        printf("idDiscipline: %d\n", q->solution.idDiscipline);
+        printf("disciplineName: %s\n", q->solution.disciplineName);
+        printf("idRoom: %d\n", q->solution.idRoom);
+        printf("roomName: %s\n", q->solution.roomName);
+        printf("studentsNumber: %d\n", q->solution.studentsNumber);
+        printf("sequence: %d\n", q->solution.sequence);
+        printf("idBeginSlot: %d\n", q->solution.idBeginSlot);
+        printf("beginTimeName: %s\n", q->solution.beginTimeName);
+        printf("idEndSlot: %d\n", q->solution.idEndSlot);
+        printf("endTimeName: %s\n", q->solution.endTimeName);
+        printf("idYear: %d\n", q->solution.idYear);
+        printf("idTerm: %d\n", q->solution.idTerm);
+        printf("idCollisionType: %d\n", q->solution.idCollisionType);
+        printf("collisionLevel: %d\n", q->solution.collisionLevel);
+        printf("collisionSize: %d\n", q->solution.collisionSize);
+        printf("\n");
+
+        q=q->prox;
+    }
+}
---
+++ entradas.h
@@ -0,0 +1,60 @@
+#ifndef _ENTRADAS_H_
+#define _ENTRADAS_H_
+
+#define TAMANHO_MAX_STRING 1000
+#define FALHA 0
+#define SUCESSO 1
+
+typedef struct {
+    int id;
+    int idSolution;
+    char solutionName[TAMANHO_MAX_STRING];
+    char solutionInitials[TAMANHO_MAX_STRING];
+    int idTeacher;
+    char teacherName[TAMANHO_MAX_STRING];
+    int idDay;
+    int idInstitution;
+    int idUnit;
+    char unitName[TAMANHO_MAX_STRING];
+    int idUnitCourse;
+    int idCourse;
+    char courseName[TAMANHO_MAX_STRING];
+    int idClass;
+    char className[TAMANHO_MAX_STRING];
+    int idDiscipline;
+    char disciplineName[TAMANHO_MAX_STRING];
+    int idRoom;
+    char roomName[TAMANHO_MAX_STRING];
+    int studentsNumber;
+    int sequence;
+    int idBeginSlot;
+    char beginTimeName[TAMANHO_MAX_STRING];
+    int idEndSlot;
+    char endTimeName[TAMANHO_MAX_STRING];
+    int idYear;
+    int idTerm;
+    int idCollisionType;
+    int collisionLevel;
+    int collisionSize;
+} DadosEntrada;
+
+DadosEntrada parseDadosEntrada(char *linha);
+
+typedef struct dado {
+    unsigned int chave;
+    DadosEntrada solution;
+    struct dado* prox;
+} dado;
+
+typedef struct noDescritor {
+    struct dado* first;
+    struct dado* last;
+    int qtdDados;
+} noDescritor;
+
+int criarLista(noDescritor** noD);
+int insOrdemRecebida(noDescritor** noD, DadosEntrada celula);
+int lerDados(noDescritor** noD);
+void printarDados(noDescritor** noD);
+
+#endif
